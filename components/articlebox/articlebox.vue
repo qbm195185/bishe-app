040941ac9d83a276aef7a6589823d18519ecaebd
全铭 <62321493+qbm195185@users.noreply.github.com>
2023-03-06 11:15:12 +0800

    Add files via upload

---
+++ components/articlebox/articlebox.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="articlebox">
+		<view class="pic">
+			<img mode="aspectFill" src="../../static/logo.png" alt="" srcset="">
+		</view>
+		<view class="text">
+			<view class="title">title</view>
+			<view class="info">
+				<text>作者</text>
+				<text>999</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"articlebox",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+	
+	.articlebox{
+		display: flex;
+	}
+	.articlebox .pic{
+		width: 230rpx;
+		height: 160rpx;
+	}
+	.pic image{
+		height: 100%;
+		width: 100%;
+	}
+	.articlebox .text{
+		border: 1px solid red;
+		flex: 1;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="home">
+		<scroll-view scroll-x class="navscroll">
+			<view class="item">帖子</view>
+			<view class="item">校园圈</view>
+			<view class="item">校园咸鱼</view>
+			<img src="../../static/search.png" alt="搜素" srcset="" class="search">
+		</scroll-view>
+		<view class="content">
+			<div class="row" v-for="item in 10">
+				<articlebox></articlebox>
+			</div>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	.navscroll {
+		height: 100rpx;
+		background-color: aliceblue;
+		text-align: center;
+	}
+	.navscroll .item {
+		font-size: 40rpx;
+		display: inline-block;
+		line-height: 100rpx;
+		padding: 0 20rpx;
+		color: #333;
+	}
+	.search{
+		position: relative;
+		right: 10rpx;
+		top: 10rpx;
+		/* size: 60rpx; */
+	}
+	.content{
+		padding: 15rpx;
+	}
+	.content .row{
+		border-bottom: 1px dashed #efeeef;
+		padding: 10px;
+	}
+</style>
